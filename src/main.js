cd2b3da9014d5bbaa3ca2178795a18b2600e44f0
ChanningKuo <492689363@qq.com>
2017-04-05 15:01:07 +0800

    添加eleme ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 开启debug模式
+// Vue.config.debug = true
+// 配置是否允许vue-devtools 检查代码  默认为true
+// Vue.config.devtools = true
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
