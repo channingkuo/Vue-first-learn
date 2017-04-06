f8e4e545ccd6f262780a28e3823e214a05ab96bb
ChanningKuo <492689363@qq.com>
2017-04-06 10:25:01 +0800

    密码和用户名对齐

--- src/module/home/login.vue
+++ src/module/home/login.vue
@@ -5,7 +5,7 @@
 			<el-form-item label="用户名" prop="username">
     			<el-input v-model="loginModel.username" auto-complete="off"></el-input>
   			</el-form-item>
-			<el-form-item label="密码" prop="password">
+			<el-form-item label="密　码" prop="password">
     			<el-input type="password" v-model="loginModel.password" auto-complete="off"></el-input>
   			</el-form-item>
 		</el-form>
