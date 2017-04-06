e56e5b1aaf5b20386bd0a698f9662f765f81b68e
edgardong@rektec.com.cn <edgardong@rektec.com.cn>
2017-04-06 08:50:09 +0800

    解决main.js冲突

--- src/main.js
+++ src/main.js
@@ -6,18 +6,9 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
 import axios from 'axios';
 
-<<<<<<< HEAD
-Vue.use(ElementUI);
 Vue.config.productionTip = false;
-=======
-// element-ui框架
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 
-Vue.use(ElementUI)
-
-Vue.config.productionTip = false
->>>>>>> 568896a09410e39bb27380958108c2d79be491b1
+Vue.use(ElementUI);
 
 // 开启debug模式
 // Vue.config.debug = true
