764214842baeb615d8ba6673aa7dc32c32717fdb
ChanningKuo <492689363@qq.com>
2017-04-07 14:01:39 +0800

    接口

--- src/module/channing/channingList.vue
+++ src/module/channing/channingList.vue
@@ -1,25 +1,22 @@
 <template>
 	<div id="channing-list">
-	    <mt-header title="Channing">
+	    <mt-header title="Channing" fixed>
 		    <router-link to="/application" slot="left">
 		        <mt-button icon="back"></mt-button>
 		    </router-link>
 	    </mt-header>
 
-        <!-- <el-input placeholder="请输入查询条件" icon="search" v-model="queryValue" :on-icon-click="searchClick"></el-input> -->
-
-        <!-- <ul v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="10" infinite-scroll-immediate-check="false">
-            <li v-for="(item, index) in listData">
-                <p>{{ item.new_title }}</p>
+        <mt-loadmore :top-method="loadMore" @top-status-change="handleTopChange" class="list-content">
+            <div v-for="item in listData">
+                <img class="image-class" :src="baseUrl + '/WeChat_Photoes/' + item.wx_photos[0].new_image_urlName" />
+                <h2>{{ item.new_title }}</h2>
                 <p>{{ item.new_content }}</p>
-                <img v-for="img in item.wx_photos" :src="baseUrl + img.new_image_urlName" />
-            </li>
-        </ul> -->
-        <div v-for="item in listData">
-            <img class="image-class" :src="baseUrl + '/WeChat_Photoes/' + item.wx_photos[0].new_image_urlName" />
-            <h2>{{ item.new_title }}</h2>
-            <p>{{ item.new_content }}</p>
-        </div>
+            </div>
+            <div slot="top" class="mint-loadmore-top">
+                <span v-show="topStatus !== 'loading'" :class="{ 'rotate': topStatus === 'drop' }">↓</span>
+                <span v-show="topStatus === 'loading'">Loading...</span>
+            </div>
+        </mt-loadmore>
 	</div>
 </template>
 
@@ -30,9 +27,10 @@
 	export default {
 		data() {
 			return {
-                // pageIndex: 1,
-                // pageSize: 10,
-                // loading: false,
+                topStatus: '',
+                pageIndex: 1,
+                pageSize: 10,
+                loading: false,
                 // queryValue: '',
                 listData: [],
                 baseUrl: ''
@@ -45,6 +43,9 @@
             })
         },
 		methods: {
+            handleTopChange(status) {
+                this.topStatus = status;
+            },
             // searchClick: function(){
 			// 	this.pageIndex = 1
 			// 	this.listData = []
@@ -56,20 +57,20 @@
 					text: '正在加载...'
 				})
 
-                let apiUrl = 'api/WxTakePhotoDemo/DataForView?entityId='
+                let apiUrl = 'api/Vue/VueList?pageSize=' + this.pageSize + '&pageIndex=' + this.pageIndex + '&entityId='
 	          	let url = localStorage.XrmBaseUrl + apiUrl
 	          	let config = {
 	          		 headers: {'Authorization': 'Basic ' + localStorage.XrmAuthToken}
 	          	}
-                console.log(this.listData)
 	            this.$http.get(url, config)
 	                .then((resp) => {
+                        this.loading = false
                         loading.close()
                         for(var i = 0; i < resp.data.length; i++){
     						this.listData.push(resp.data[i])
     					}
-                        console.log(this.listData)
 	                }, (err) => {
+                        this.loading = false
                         loading.close()
     					Toast({
     						message: err.response.data
@@ -78,12 +79,12 @@
     						// duration: 3000
     					})
 	                })
+            },
+            loadMore() {
+                this.loading = true
+				this.pageIndex += 1
+                this.loadData()
             }
-            // loadMore() {
-            //     this.loading = true
-			// 	this.pageIndex += 1
-            //     this.loadData()
-            // }
 		}
 	}
 </script>
@@ -91,5 +92,9 @@
 <style scoped>
 .image-class{
     width: 200px;
+}
+
+.list-content{
+    margin-top: 40px;
 }
 </style>
